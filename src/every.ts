a470a4a3e22d53990f2e50ded644edd16f03c41a
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2021-11-28 15:23:14 +0100

    Add comments for some and every

--- src/every.ts
+++ src/every.ts
@@ -1,5 +1,11 @@
 import { TransformStream } from "isomorphic-streams";
 
+/**
+ * Determines whether every chunk of the stream fulfills a given predicate.
+ *
+ * @param predicate - The predicate which is applied on each chunk
+ * @returns A `TransformStream` through which a `ReadableStream` can be piped
+ */
 export function every<I>(predicate: (value: I) => boolean): TransformStream<I, boolean> {
   return new TransformStream<I, boolean>({
     transform(chunk, controller) {
--- src/some.ts
+++ src/some.ts
@@ -1,5 +1,11 @@
 import { TransformStream } from "isomorphic-streams";
 
+/**
+ * Determines whether some chunks of the stream fulfill a given predicate.
+ *
+ * @param predicate - The predicate which is applied on each chunk
+ * @returns A `TransformStream` through which a `ReadableStream` can be piped
+ */
 export function some<I>(predicate: (value: I) => boolean): TransformStream<I, boolean> {
   return new TransformStream<I, boolean>({
     transform(chunk, controller) {
